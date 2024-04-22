b5832ba53cd55b6d2e70525468d7ba2262a5d611
DariaHasin <dhasin1995@gmail.com>
2024-04-22 11:44:23 +0300

    fix normalizeFlow and add showOpticalFlowBetweenSlices

--- optical-flow/lk.py
+++ optical-flow/lk.py
@@ -1,7 +1,8 @@
 import SimpleITK as sitk
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import cv2
+import math
 
 ################################## OpticalFlowPyrLK ##################################
 def drawArrows(image, flow, features):
@@ -38,17 +39,29 @@ def showOpticalFlowPyrLKTwoDirections(slice1, slice2):
     cv2.destroyAllWindows()
 
 ################################ OpticalFlowFarneback ################################
+# def normalizeFlow(flow):
+#     magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+#     magnitude = cv2.normalize(magnitude, None, 0.0, 1.0, cv2.NORM_MINMAX)
+#     hue = (angle + np.pi) * 180 / (2 * np.pi)
+#     hsv = cv2.cvtColor(magnitude[..., None], cv2.COLOR_GRAY2BGR)
+#     hsv = cv2.cvtColor(hsv, cv2.COLOR_BGR2HSV)
+#     hsv[..., 0] = hue
+#     hsv[..., 1] = 255
+#     hsv[..., 2] = magnitude
+#     flow_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+#     flow_img = flow_img.astype(np.uint8)
+#     return flow_img
+
+
 def normalizeFlow(flow):
     magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-    magnitude = cv2.normalize(magnitude, None, 0.0, 1.0, cv2.NORM_MINMAX)
-    hue = angle * 180 / np.pi / 2
-    hsv = cv2.cvtColor(magnitude[..., None], cv2.COLOR_GRAY2BGR)
-    hsv = cv2.cvtColor(hsv, cv2.COLOR_BGR2HSV)
+    magnitude = cv2.normalize(magnitude, None, 0.0, 255, cv2.NORM_MINMAX)
+    hue = ((angle + np.pi) * 180 / (2 * np.pi)).astype(np.uint8)
+    hsv = np.zeros((flow.shape[0], flow.shape[1], 3), dtype=np.uint8)
     hsv[..., 0] = hue
     hsv[..., 1] = 255
     hsv[..., 2] = magnitude
     flow_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-    flow_img = flow_img.astype(np.uint8)
     return flow_img
 
 
@@ -57,33 +70,61 @@ def showOpticalFlowFarneback(slice1, slice2):
                                         flow=None, pyr_scale=0.5, 
                                         levels=3, winsize=15, 
                                         iterations=15, poly_n=7, 
-                                        poly_sigma=1.5, flags=0)
+                                        poly_sigma=1.5, flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
+    
     flow_img = normalizeFlow(flow)
-    flow_img_color = cv2.applyColorMap(flow_img, cv2.COLORMAP_TURBO)
-
-    image_bright = cv2.convertScaleAbs(slice2, alpha=3, beta=20)
+    image_bright = cv2.convertScaleAbs(slice2, alpha=4, beta=30)
     image_color = cv2.cvtColor(image_bright, cv2.COLOR_GRAY2BGR)
 
-    alpha = 0.4
-    overlay = cv2.addWeighted(flow_img_color, alpha, image_color, 1-alpha, 0)
+    alpha = 0.7
+    overlay = cv2.addWeighted(flow_img, alpha, image_color, 1-alpha, 0)
     cv2.imshow('Flow Visualization', overlay)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def showOpticalFlowBetweenSlices(slice1, slice2, slice3, i):
+    flow1 = cv2.calcOpticalFlowFarneback(prev=slice1, next=slice2, 
+                                          flow=None, pyr_scale=0.5, 
+                                          levels=3, winsize=15, 
+                                          iterations=15, poly_n=7, 
+                                          poly_sigma=1.5, flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
+    
+    flow2 = cv2.calcOpticalFlowFarneback(prev=slice3, next=slice2, 
+                                          flow=None, pyr_scale=0.5, 
+                                          levels=3, winsize=15, 
+                                          iterations=15, poly_n=7, 
+                                          poly_sigma=1.5, flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
+    
+    flow_img1 = normalizeFlow(flow1)
+    flow_img2 = normalizeFlow(flow2)
+    
+    image_bright = cv2.convertScaleAbs(slice2, alpha=4, beta=30)
+    image_color = cv2.cvtColor(image_bright, cv2.COLOR_GRAY2BGR)
+
+    alpha = 0.7
+    overlay1 = cv2.addWeighted(flow_img1, alpha, image_color, 1-alpha, 0)
+    overlay2 = cv2.addWeighted(flow_img2, alpha, image_color, 1-alpha, 0)
+
+    horizontal_concat = cv2.hconcat([overlay1, overlay2])
+    cv2.imshow(f'Flow Visualization Slice {i} (Left {i-1}->{i}, Right {i+1}->{i})', horizontal_concat)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 ######################################################################################
 
 if __name__ == '__main__':
-    image_path = r"\\10.101.119.14\Dafna\Daria\scans_stripped\Daria\ax_stripped.nii.gz"
+    image_path = "/Users/dariahasin/Library/CloudStorage/GoogleDrive-dhasin1995@gmail.com/My Drive/TAU/tof-project/scans_stripped/Daria/ax_stripped.nii.gz"
     image = sitk.ReadImage(image_path)
     image_arr = sitk.GetArrayFromImage(image)
     image_arr_8bit = cv2.normalize(image_arr, None, 255, 0, cv2.NORM_MINMAX, cv2.CV_8U)
 
     shape = image_arr_8bit.shape
     for i in range(30, shape[0]-1):
-        slice1 = image_arr_8bit[i]
-        slice2 = image_arr_8bit[i+1]
+        slice1 = image_arr_8bit[i-1]
+        slice2 = image_arr_8bit[i]
+        slice3 = image_arr_8bit[i+1]
         # showOpticalFlowPyrLKTwoDirections(slice1, slice2)
-        showOpticalFlowFarneback(slice1, slice2)
-
-
-
-
+        # showOpticalFlowFarneback(slice1, slice2)
+        showOpticalFlowBetweenSlices(slice1, slice2, slice3, i)
+        # break
